6b9bc5342291dfa1a9626180dcde600f4d6c37e8
DarioGiusino <114605234+DarioGiusino@users.noreply.github.com>
2023-11-21 11:40:27 +0100

    Add files via upload

---
+++ Laravel/laravel-dc-comics/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ComicController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// * home route
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// * comics routes
+// Route::resource('comics', ComicController::class);
+
+// route for comics list
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+
+// route for create new comic form
+Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
+
+// route for comic detail
+Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
+
+// route to store a new comic in db
+Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
+
+// route to get the form for edit the resource
+Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
+
+// route to save the edit in db
+Route::put('comics/{comic}', [ComicController::class, 'update'])->name('comics.update');
+
+// route to destroy a resource from the db
+Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
