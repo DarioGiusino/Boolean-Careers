d6e237436f056795f861627e8759422cfb8b3569
DarioGiusino <114605234+DarioGiusino@users.noreply.github.com>
2023-11-21 11:48:58 +0100

    Add files via upload

---
+++ Laravel/laravel-primi-passi/resources/views/second.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Secondi Passi</title>
+</head>
+
+<body>
+    <h1>hello earth</h1>
+    <a href="{{route('first')}}">Vai ad un'altra pagina</a>
+
+</body>
+
+</html>
